27a3b7e15f1921b38d2be42ea51d7a33ffc8e960
Matheus Serra <matheus.serra.7798@gmail.com>
2025-06-20 14:39:25 -0300

    Adiciona a escolha das posições para células do tipo água.
    - As posições das células de água podem ser definidas no "Inspector".
    - Todas as células que não forem do tipo água serão por padrão do tipo árvore.
    - Implementada a visualização das células de água usando "Gizmos".

--- Assets/Scripts/ControladorDeGrid.cs
+++ Assets/Scripts/ControladorDeGrid.cs
@@ -1,4 +1,6 @@
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ControladorDeGrid : MonoBehaviour
 {
@@ -7,6 +9,7 @@ public class ControladorDeGrid : MonoBehaviour
     [SerializeField] private GameObject prefabArvore;
     [SerializeField] private GameObject prefabAgua;
     [SerializeField] private Vector2 posicaoInicial;
+    [SerializeField] private Vector2Int[] posicoesDeAgua;
     private Celula[,] gridArray;
 
     private void Start()
@@ -22,15 +25,37 @@ public class ControladorDeGrid : MonoBehaviour
         {
             for (int j = 0; j < linhasGrid; j++)
             {
+                TipoDeCelula tipo = TipoDeCelula.Arvore;
+                GameObject prefab = prefabArvore;
+
+                if (PosicaoDeAgua(i + 1, j + 1))
+                {
+                    tipo = TipoDeCelula.Agua;
+                    prefab = prefabAgua;
+                }
+
                 Vector2 posicao = new(posicaoInicial.x + i, posicaoInicial.y + j);
-                GameObject objCelula = Instantiate(prefabArvore, posicao, Quaternion.identity);
-                objCelula.name = $"Arvore({i}, {j})";
+                GameObject objCelula = Instantiate(prefab, posicao, Quaternion.identity);
+                objCelula.name = $"{tipo}({i}, {j})";
                 objCelula.transform.parent = transform;
 
-                Celula celula = new(new Vector2Int(i, j), TipoDeCelula.Arvore, objCelula);
+                Celula celula = new(new Vector2Int(i, j), tipo, objCelula);
                 gridArray[i, j] = celula;
             }
         }
+    }
+
+    private bool PosicaoDeAgua(int x, int y)
+    {
+        foreach (var pos in posicoesDeAgua)
+        {
+            if (pos.x == x && pos.y == y)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     private void OnDrawGizmosSelected()
@@ -43,6 +68,17 @@ public class ControladorDeGrid : MonoBehaviour
             {
                 Vector3 pos = new Vector3(posicaoInicial.x + i, posicaoInicial.y + j, 0);
                 Gizmos.DrawWireCube(pos, Vector3.one);
+            }
+        }
+
+        if (posicoesDeAgua != null)
+        {
+            Gizmos.color = Color.blue;
+
+            foreach (var pos in posicoesDeAgua)
+            {
+                Vector3 position = new(posicaoInicial.x + pos.x, posicaoInicial.y + pos.y, 0);
+                Gizmos.DrawCube(position, Vector3.one * 0.8f);
             }
         }
     }
