a85ea9bdf67b49ddc403a8d9d995c24cc684826a
Matheus Serra <matheus.serra.7798@gmail.com>
2025-06-21 01:33:52 -0300

    Adiciona movimentação básica do jogador através do grid.
    - Erro na visualização do grid por meio dos "Gizmos" corrigido.
    - Célula inicial e tempo de duração da movimentação do jogador
      podem ser definidos no "Inspector".
    - Jogador só se move por células que existem e que contém árvores.

--- Assets/Scripts/ControladorDeGrid.cs
+++ Assets/Scripts/ControladorDeGrid.cs
@@ -1,18 +1,19 @@
-using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class ControladorDeGrid : MonoBehaviour
 {
+    [SerializeField] private Vector2 posicaoInicial;
+    public Vector2 PosicaoInicial => posicaoInicial;
     [SerializeField] private int colunasGrid;
+    public int ColunasGrid => linhasGrid;
     [SerializeField] private int linhasGrid;
+    public int LinhasGrid => linhasGrid;
     [SerializeField] private GameObject prefabArvore;
     [SerializeField] private GameObject prefabAgua;
-    [SerializeField] private Vector2 posicaoInicial;
     [SerializeField] private Vector2Int[] posicoesDeAgua;
     private Celula[,] gridArray;
 
-    private void Start()
+    private void Awake()
     {
         GerarGrid();
     }
@@ -58,6 +59,13 @@ public class ControladorDeGrid : MonoBehaviour
         return false;
     }
 
+    public Celula PegarCelula(int x, int y)
+    {
+        if (x < 0 || x >= colunasGrid || y < 0 || y >= linhasGrid) return null;
+
+        return gridArray[x, y];
+    }
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
@@ -66,7 +74,7 @@ public class ControladorDeGrid : MonoBehaviour
         {
             for (int j = 0; j < linhasGrid; j++)
             {
-                Vector3 pos = new Vector3(posicaoInicial.x + i, posicaoInicial.y + j, 0);
+                Vector3 pos = new(posicaoInicial.x + i, posicaoInicial.y + j, 0);
                 Gizmos.DrawWireCube(pos, Vector3.one);
             }
         }
@@ -77,7 +85,7 @@ public class ControladorDeGrid : MonoBehaviour
 
             foreach (var pos in posicoesDeAgua)
             {
-                Vector3 position = new(posicaoInicial.x + pos.x, posicaoInicial.y + pos.y, 0);
+                Vector3 position = new(posicaoInicial.x - 1 + pos.x, posicaoInicial.y - 1 + pos.y, 0);
                 Gizmos.DrawCube(position, Vector3.one * 0.8f);
             }
         }
---
+++ Assets/Scripts/MovimentacaoDoJogador.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using UnityEngine;
+
+public class MovimentacaoDoJogador : MonoBehaviour
+{
+    [SerializeField] private ControladorDeGrid diretorDeGrid;
+    [SerializeField] private Vector2Int posicaoInicial;
+    [SerializeField] private float tempoDeMovimento;
+    private Vector2Int posicaoAtual;
+    private bool movendo = false;
+
+    private void Start()
+    {
+        posicaoAtual = new(posicaoInicial.x - 1, posicaoInicial.y - 1);
+        transform.position = new(diretorDeGrid.PosicaoInicial.x + posicaoAtual.x,
+                                  diretorDeGrid.PosicaoInicial.y + posicaoAtual.y,
+                                  0);
+    }
+
+    void Update()
+    {
+        Vector2Int direcao = Vector2Int.zero;
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            direcao = Vector2Int.up;
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            direcao = Vector2Int.down;
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            direcao = Vector2Int.left;
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            direcao = Vector2Int.right;
+
+        if (direcao != Vector2Int.zero)
+        {
+            TentarMover(direcao);
+        }
+    }
+
+    IEnumerator IrParaCelula(Vector2Int posicao)
+    {
+        movendo = true;
+        Vector3 origem = transform.position;
+        Vector3 destino = new(diretorDeGrid.PosicaoInicial.x + posicao.x,
+                                  diretorDeGrid.PosicaoInicial.y + posicao.y,
+                                  0);
+        float cronometro = 0f;
+
+        while (cronometro < tempoDeMovimento)
+        {
+            cronometro += Time.deltaTime;
+            transform.position = Vector3.Lerp(origem, destino, cronometro / tempoDeMovimento);
+            cronometro += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = destino;
+        posicaoAtual = posicao;
+
+        movendo = false;
+    }
+
+    private void TentarMover(Vector2Int direcao)
+    {
+        if (movendo) return;
+
+        Vector2Int posicaoAlvo = posicaoAtual + direcao;
+
+        if (!CelulaExiste(posicaoAlvo)) return;
+
+        Celula celulaAlvo = diretorDeGrid.PegarCelula(posicaoAlvo.x, posicaoAlvo.y);
+
+        if (celulaAlvo != null && celulaAlvo.tipo == TipoDeCelula.Arvore)
+        {
+            StartCoroutine(IrParaCelula(posicaoAlvo));
+        }
+    }
+
+    private bool CelulaExiste(Vector2Int posicao)
+    {
+        return posicao.x >= 0 && posicao.x < diretorDeGrid.ColunasGrid &&
+               posicao.y >= 0 && posicao.y < diretorDeGrid.LinhasGrid;
+    }
+}
